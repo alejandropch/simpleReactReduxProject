fbbfac51b79e1107f67ca5f12ab0ef25d2b31f50
alejandropch <alejandropachas1@gmail.com>
2021-09-02 18:07:52 -0500

    using useDispatch and useSelector to use the state in jsx code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import React from 'react'
+import {useSelector, useDispatch} from 'react-redux'
+import {increase,decrease} from './actions'
 
 function App() {
+
+  const counter=useSelector(state=>state.counter)
+  const dispatch=useDispatch();
   return (
     <div className="App">
       <header className="App-header">
        <h1>Hi amigo</h1>
+       <p>This is the counter <i>{counter}</i></p>
+       <button onClick={()=>dispatch(increase())}>+</button>
+       <button onClick={()=>dispatch(decrease())}>-</button>
+
+
       </header>
     </div>
   );
