f74dda8641018197e4429093fba8ecadbe7944f4
alejandropch <alejandropachas1@gmail.com>
2021-09-02 16:40:45 -0500

    adding the reducer

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createStore} from 'redux';
 
 
 //actions
@@ -13,9 +14,21 @@ const increase=()=>{
   }
 }
 
+//reducer
+const counter = (state = 0,action)=>{
+
+  switch (action.type) {
+    case 'INCREASE':
+      return state+1;
+  
+    default:
+      break;
+  }
+}
+
+
+
 ReactDOM.render(<App />,document.getElementById('root'));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
